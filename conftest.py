f54e5cfbd5f6d2a8afd61cb2a03e00e0b75f3065
Eugeny Okulik <tiburon854@gmail.com>
2023-07-12 08:22:33 +0300

    Configured directory for profile

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def driver():
+    options = Options()
+    options.add_argument('user-data-dir=avby-test')
     chrome_driver = webdriver.Chrome()
     chrome_driver.maximize_window()
     return chrome_driver
--- tests/test_search_cat.py
+++ tests/test_search_cat.py
@@ -1,8 +1,10 @@
 from pages.search import SearchPage
 from pages.results import ResultsPage
+import pytest
 
 
-def test_cat_search(driver):
+@pytest.mark.parametrize('n', range(50))
+def test_cat_search(driver, n):
     search_page = SearchPage(driver)
     search_page.open_page()
     search_page.enter_search_phrase('cat')
